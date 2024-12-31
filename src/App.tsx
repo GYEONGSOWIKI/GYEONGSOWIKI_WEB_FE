8e2bcf73ee90b23c0c2c25ffd369a855aafc6e27
806gw <shjea4364@naver.com>
2024-12-31 12:05:15 +0900

    🚨 create warning message - 💄 change style home

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,13 @@
 import * as R from "./allFiles";
 
+import { useEffect } from "react";
+
 const App = () => {
+  useEffect(() => {
+    console.log('%c ⚠️ 타인에 대한 조롱, 또는 비방, 비난으로 인해 발생하는 법적인 문제에 대한 책임은 본인에게 있습니다.',
+      `font-size:30px; font-weight: 900; color:white; background-color: red`);
+  }, [])
+
   return (
     <R.CommonRouter />
   );
